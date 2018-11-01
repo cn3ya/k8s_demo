26947d16b9e342db7b5131322f86269969f5aed2
Liangjw01 <Liangjw01@mingyuanyun.com>
2018-11-01 17:57:50 +0800

    add trace header

--- images/service-c/index.js
+++ images/service-c/index.js
@@ -21,17 +21,30 @@ app.get('/waiting', (req, res) => setTimeout(function () {
 app.get('/call', (req, res) => {
   let services = req.query.services || '';
   let servicesArray = services.split(',');
-  // res.send(JSON.stringify(servicesArray));
   if (servicesArray.length > 0 ) {
     const service = servicesArray.shift();
     const serviceArray = service.split(':');
     services = servicesArray.join();
+    let headers = {};
+    [
+      'x-request-id',
+      'x-b3-traceid',
+      'x-b3-spanid',
+      'x-b3-parentspanid',
+      'x-b3-sampled',
+      'x-b3-flags',
+      'x-ot-span-context'
+    ].forEach(item => {
+      if (req.get(item)) {
+        headers[item] = req.get(item)
+      }
+    });
     http.get({
       protocol: 'http:',
       host: serviceArray[0],
       port: serviceArray[1],
       path: `/${serviceArray[2]}?services=${services}`,
-      headers:req.headers
+      headers: headers
     }, (resp) => {
       let data = '';
       // A chunk of data has been recieved.
